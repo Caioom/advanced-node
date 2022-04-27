f717579ae4923654d9600e432704399f96a400f8
caioom <caio.ds.2003@gmail.com>
2022-04-27 20:51:00 -0300

    ensure service returns AuthenticationError when LoadFacebookUserApi returns undefined

--- tests/data/facebook-authentication.spec.ts
+++ tests/data/facebook-authentication.spec.ts
@@ -1,28 +1,34 @@
 import { FacebookAuthentication } from '@/domain/features'
+import { AuthenticationError } from '@/domain/errors'
 
 class FacebookAuthenticationService {
   constructor (private readonly loadFacebookUserApiApi: LoadFacebookUserApi) {}
 
-  async perform (params: FacebookAuthentication.Params): Promise<void> {
+  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     await this.loadFacebookUserApiApi.load(params)
+    return new AuthenticationError()
   }
 }
 
 interface LoadFacebookUserApi {
-  load: (token: LoadFacebookUserApi.Params) => Promise<void>
+  load: (token: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
 }
 
 namespace LoadFacebookUserApi {
   export type Params = {
     token: string
   }
+
+  export type Result = undefined
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  result = undefined
 
-  async load (params: LoadFacebookUserApi.Params): Promise<void> {
+  async load (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -34,5 +40,15 @@ describe('FacebookAuthenticationService', () => {
     await sut.perform({ token: 'any_string' })
 
     expect(loadFacebookUserApi.token).toBe('any_string')
+  })
+
+  it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_string' })
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
