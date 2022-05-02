b88c3e66af587f2b2884228612a9af219466a2d7
caioom <caio.ds.2003@gmail.com>
2022-05-01 22:43:23 -0300

    ensure FacebookApi gets a facebook user

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -18,6 +18,7 @@ describe('FacebookApi', () => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   }) 
 
@@ -55,6 +56,16 @@ describe('FacebookApi', () => {
         access_token: 'any_client_token',
         fields: 'id,name,email'
       }
+    })
+  })
+
+  it('should return facebook user', async () => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      email: 'any_fb_email',
+      name: 'any_fb_name'
     })
   })
 })
